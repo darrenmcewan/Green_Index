4e7e362b7876ce542cfe0f9458d1135937528065
Darren McEwan <github@darrenmcewan.com>
2023-02-02 23:07:35 +0000

    added map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import leafmap
 
 st.header("Demo App for Wind Visualizations")
 st.subheader("Showcasing potential wind farm locations")
+
+m = leafmap.Map(minimap_control=True)
+m.add_basemap("OpenTopoMap")
+m.to_streamlit(height=500)
