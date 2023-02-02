d23257da935dd141418170e39cf37df30c9c9382
Darren McEwan <github@darrenmcewan.com>
2023-02-02 23:50:35 +0000

    seperated

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,12 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 st.header("Demo App for Wind Visualizations")
-st.subheader("Showcasing potential wind farm locations")
+st.text("Quick demo that utilizes wind data from https://data.nrel.gov/submissions/54 and leafmap")
 
 filepath = "wtk_site_metadata.csv"
 USbounds = [-124.848974, 24.396308,-66.885444, 49.384358]
 
+st.header("Wind Speed")
 m = leafmap.Map()
 m.add_heatmap(
     filepath,
@@ -18,15 +19,18 @@ m.add_heatmap(
     name="Wind Speed",
     radius=20
 )
+m.zoom_to_bounds(USbounds)
+m.to_streamlit(height=700)
+
+st.header("Capacity Factor")
+m = leafmap.Map()
 m.add_heatmap(
     filepath,
     latitude="latitude",
     longitude='longitude',
     value="capacity_factor",
     name="Capacity Factor",
-    radius=20,
-    add_legend=True
+    radius=20
 )
 m.zoom_to_bounds(USbounds)
 m.to_streamlit(height=700)
-m.save_draw_features('features.txt')
