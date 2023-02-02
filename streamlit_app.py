6ad28fa76f845fcac84fa50828670cd05418a0a7
Darren McEwan <github@darrenmcewan.com>
2023-02-02 22:59:38 +0000

    Importing libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit as st
+import leafmap.foliumap as leafmap
 
-st.header("test")
+st.header("Demo App for Wind Visualizations")
+st.subheader("Showcasing potential wind farm locations")
