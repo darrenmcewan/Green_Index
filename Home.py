039d78ff7778ed5e375b0d7aa42f163d8f14bd5e
darrenmcewan <github@darrenmcewan.com>
2023-02-23 20:01:17 -0800

    update

---
+++ Home.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+import base64
+from pathlib import Path
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to the Green Index! 🌲")
+st.subheader("Making Informed Decisions by Visualizing, Exploring, and Evaluating Renewable Energy Data")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Darren McEwan, Isabel Chang, Anjing Bi, Hanh Pham, Edmund Dale, Kelly Seto 
+    """
+)
+
+with st.columns(3)[1]:
+    image = Image.open("images/GT_logo.png")
+    image = image.resize((200,200))
+    st.image(image)
