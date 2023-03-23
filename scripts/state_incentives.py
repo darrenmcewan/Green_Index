015be9e8d5bfa1413c0abaac9fef940b37a6e834
Darren McEwan <damcewan@cisco.com>
2023-03-23 11:21:44 -0700

    fix link bug

--- scripts/state_incentives.py
+++ scripts/state_incentives.py
@@ -1,16 +1,16 @@
 def show_resources(state='AK'):
     import pandas as pd
     def make_clickable(link):
-        link = link.split('#')[1]
+        url = link.split('#')[1]
         name = link.split('#')[0]
 
-        return f'<a target="_blank" href="{name}">{link}</a>'
+        return f'<a target="_blank" href="{url}">{name}</a>'
 
     data = pd.read_csv('data/state_incentives.csv', index_col=False)
     data = data[data['State'] == state]
     data['name_url'] = data['Name'] + "#" + data['URL']
-    data['Program Link'] = data['name_url'].apply(make_clickable)
-    data = data.iloc[:, [0, 3, 4, 5, 7]]
+    data['Program Name'] = data['name_url'].apply(make_clickable)
+    data = data.iloc[:, [7, 3, 4, 5]]
     data = data.to_html(escape=False)
 
     return data
